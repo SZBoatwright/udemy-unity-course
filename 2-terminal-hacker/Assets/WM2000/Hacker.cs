f22bd709c6d7a2af79ca167e45a0aad1107881ee
Zach <szboatwright@gmail.com>
2018-04-18 18:54:31 -0500

    📦  NEW: adds Hacker.cs from lectures 12-14

---
+++ 2-terminal-hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu();
+	}
+
+	void ShowMainMenu () {
+		Terminal.ClearScreen();
+		var greeting = "Hello Zach";
+		Terminal.WriteLine(greeting);
+		Terminal.WriteLine("L33T H3CK0r 3000");
+		Terminal.WriteLine("WH3T D0 U W3NN3 H3K IN2???:");
+		Terminal.WriteLine("1 - Miss Edge's Laptop");
+		Terminal.WriteLine("2 - Popo station");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
