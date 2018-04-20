5362e6a349d0824ca21d85866c6a7121402d393b
Zach <szboatwright@gmail.com>
2018-04-20 15:54:01 -0500

    📦  NEW: adds onUserInput section

--- 2-terminal-hacker/Assets/WM2000/Hacker.cs
+++ 2-terminal-hacker/Assets/WM2000/Hacker.cs
@@ -11,16 +11,37 @@ public class Hacker : MonoBehaviour {
 
 	void ShowMainMenu () {
 		Terminal.ClearScreen();
-		var greeting = "Hello Zach";
-		Terminal.WriteLine(greeting);
+		Terminal.WriteLine("Hello, Zach");
 		Terminal.WriteLine("L33T H3CK0r 3000");
 		Terminal.WriteLine("WH3T D0 U W3NN3 H3K IN2???:");
-		Terminal.WriteLine("1 - Miss Edge's Laptop");
+		Terminal.WriteLine("1 - Miss Edge's laptop");
 		Terminal.WriteLine("2 - Popo station");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnUserInput(string input)
+	{
+		if (input == "1") {
+			Terminal.WriteLine("Attempting to hack into Miss Edge's ");
+			Terminal.WriteLine("laptop... ");
+			Terminal.WriteLine("... (this should be easy) ...");
+		}
+		else if (input == "2") {
+			Terminal.WriteLine("Attempting to hack into the popo");
+			Terminal.WriteLine("station...");
+		}
+		else if (input == "menu") {
+			ShowMainMenu();
+		}
+		else if (input == "poop") {
+			Terminal.WriteLine("💩");
+		}
+		else {
+			Terminal.WriteLine("Please select a level");
+		}
 	}
 }
