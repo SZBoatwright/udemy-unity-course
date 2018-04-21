514f6ee8845274af1726135d32e72d0c48401628
Zach <szboatwright@gmail.com>
2018-04-21 17:11:54 -0500

    📦  NEW: lecture 41 - adds input binding for boosting and rotating, but they don't do anything

---
+++ 3-project-boost/Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left lol");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right lol");
+        }
+
+	}
+}
+
