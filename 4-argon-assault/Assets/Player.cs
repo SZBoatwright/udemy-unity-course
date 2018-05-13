75c2b15f67939a24789bbe88a6a34d2c9d3e8551
Zach Boatwright <szboatwright@gmail.com>
2018-05-13 14:47:18 -0500

    📦  NEW: [Argon Assault] player now rotates when moving about the screen

--- 4-argon-assault/Assets/Player.cs
+++ 4-argon-assault/Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -9,6 +10,14 @@ public class Player : MonoBehaviour {
     [Tooltip("How far the player can move left/right")][SerializeField] float maxXMovement = 6f;
     [Tooltip("How far the player can move up/down")] [SerializeField] float maxYMovement = 3f;
 
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlYawFactor = 30f;
+    [SerializeField] float controlRollFactor = -30f;
+
+    float xThrow, yThrow;
+
     // Use this for initialization
     void Start () {
 		
@@ -17,27 +26,35 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        HandleXMovement();
-        HandleYMovement();
+        HandleTranslation();
+        HandleRotation();
     }
 
-    private void HandleYMovement()
+    private void HandleRotation()
     {
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");      // same as HandleXMovement function except for y axis
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow =  yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yawDueToControlThrow = xThrow * controlYawFactor;
+        float yaw = yawDueToPosition + yawDueToControlThrow;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); //this is a static function that returns a quaternion for what we want to rotate to
+    }
+
+    private void HandleTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");    // get x throw value from the controller input
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = xThrow * Time.deltaTime * shipSpeed;                  // determine the amount to move the player this 
         float yOffset = yThrow * Time.deltaTime * shipSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;               // determine exact position in local space the player will be based on the movements this frame
         float rawYPos = transform.localPosition.y + yOffset;
+        float newXPos = Mathf.Clamp(rawXPos, -maxXMovement, maxXMovement); // clamp the x position to the predefined max values so player stays onscreen
         float newYPos = Mathf.Clamp(rawYPos, -maxYMovement, maxYMovement);
 
-        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, transform.localPosition.z);
-    }
-
-    private void HandleXMovement()
-    {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");    // get x throw value from the controller input
-        float xOffset = xThrow * Time.deltaTime * shipSpeed;                  // determine the amount to move the player this 
-        float rawXPos = transform.localPosition.x + xOffset;               // determine exact position in local space the player will be based on the movements this fram
-        float newXPos = Mathf.Clamp(rawXPos, -maxXMovement, maxXMovement); // clamp the x position to the predefined max values so player stays onscreen
-
         transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, transform.localPosition.z);
     }
 }
