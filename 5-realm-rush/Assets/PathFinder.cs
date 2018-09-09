0c59c1893b0b3f00d120e470734675c504c75ee5
zach <szboatwright@gmail.com>
2018-09-08 19:01:16 -0500

    📦  NEW: [Realm Rush] blocks loaded into dictionary for future pathfinding use

---
+++ 5-realm-rush/Assets/PathFinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+	void Start () {
+        LoadBlocks();
+	}
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("skipped duplicate block: " + waypoint);
+            } else
+            {
+            grid.Add(gridPos, waypoint);
+            }
+        }
+        print("loaded " + grid.Count + " blocks");
+    }
+    
+    void Update () {
+		
+	}
+}
