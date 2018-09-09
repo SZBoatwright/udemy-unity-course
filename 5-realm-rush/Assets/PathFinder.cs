3f48dde6ae4efaab55e848a13e1a9e8afc33656d
zach <szboatwright@gmail.com>
2018-09-08 21:10:05 -0500

    📦  NEW: [Realm Rush] change the color of blocks when they are added to grid and change start/end point colors

--- 5-realm-rush/Assets/PathFinder.cs
+++ 5-realm-rush/Assets/PathFinder.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour {
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    [SerializeField] Waypoint startPoint, endPoint;
 
-	void Start () {
+    void Start () {
         LoadBlocks();
+        ColorStartAndEnd();
 	}
+
+    private void ColorStartAndEnd()
+    {
+        startPoint.SetTopColor(Color.blue);
+        endPoint.SetTopColor(Color.red);
+    }
 
     private void LoadBlocks()
     {
@@ -17,16 +25,16 @@ public class PathFinder : MonoBehaviour {
         foreach (Waypoint waypoint in waypoints)
         {
             var gridPos = waypoint.GetGridPos();
-            
+
             if (grid.ContainsKey(gridPos))
             {
                 Debug.LogWarning("skipped duplicate block: " + waypoint);
             } else
             {
-            grid.Add(gridPos, waypoint);
+                grid.Add(gridPos, waypoint);
+                waypoint.SetTopColor(Color.black);
             }
         }
-        print("loaded " + grid.Count + " blocks");
     }
     
     void Update () {
