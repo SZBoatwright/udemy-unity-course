66d3cc64a90c5922560a3cfbe60889264bf68778
zach <szboatwright@gmail.com>
2018-09-17 18:11:35 -0500

    📦  NEW: [Realm Rush] add tower prefab with enemy LookAt script

---
+++ 5-realm-rush/Assets/LookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+    [SerializeField] GameObject pivotObject;
+    [SerializeField] GameObject lookAtTarget;
+	
+	// Update is called once per frame
+	void Update () {
+        pivotObject.transform.LookAt(lookAtTarget.transform);
+	}
+}
