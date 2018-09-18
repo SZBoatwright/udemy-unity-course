fe0d6a45d98bd0c70dac330c2290369bfa233a24
zach <szboatwright@gmail.com>
2018-09-18 18:43:34 -0500

    📦  NEW: [Realm Rush] enemy takes damage when bullets collide, and is destroyed when health reaches 0

---
+++ 5-realm-rush/Assets/EnemyDamage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    [SerializeField] GameObject damager;
+    [SerializeField] int enemyHealth = 5;
+
+    [SerializeField] ParticleSystem deathFX;
+    [SerializeField] ParticleSystem hitFX;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other == damager)
+        {
+            enemyHealth -= 1;
+            hitFX.Play();
+
+            if (enemyHealth <= 0)
+            {
+                KillEnemy();
+            }
+        }    
+    }
+
+    private void KillEnemy()
+    {
+        print("HE DEED!!!!1");
+        deathFX.Play();
+        //sound effect
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
