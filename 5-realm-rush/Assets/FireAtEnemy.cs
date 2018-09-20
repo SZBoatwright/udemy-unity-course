348e5b82c487f87cb1b65ff7e0d954ff8b9a27e1
zach <szboatwright@gmail.com>
2018-09-20 18:57:12 -0500

    ⚡  IMPROVE: [Realm Rush] towers now have max fire distance

---
+++ 5-realm-rush/Assets/FireAtEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAtEnemy : MonoBehaviour {
+
+    [SerializeField] GameObject pivotObject;
+    [SerializeField] GameObject enemy;
+    [SerializeField] float maxShootDistance;
+    [SerializeField] ParticleSystem beam;
+
+    float enemyDistance;
+    
+    void Update ()
+    {
+        FireAtEnemies();
+    }
+
+    private void FireAtEnemies()
+    {
+        if (enemy)
+        {
+            enemyDistance = Vector3.Distance(enemy.transform.position, gameObject.transform.position);
+        }
+        if (enemy && enemyDistance < maxShootDistance)
+        {
+            pivotObject.transform.LookAt(enemy.transform);
+            beam.Play();
+        }
+        else
+        {
+            pivotObject.transform.rotation = Quaternion.Euler(Vector3.forward);
+            beam.Pause();
+        }
+    }
+}
