cfda990d357c5f4ace9c37d750946a03e47f1125
zach <szboatwright@gmail.com>
2018-09-21 18:47:04 -0500

    📦  NEW: [Realm Rush] added enemy spawner script

---
+++ 5-realm-rush/Assets/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] EnemyMovement enemy;
+    [SerializeField] float secondsBetweenSpawn = 1.75f;
+    
+	void Start () {
+        StartCoroutine(spawnEnemy());
+	}
+
+    IEnumerator spawnEnemy ()
+    {
+        while (true)
+        {
+            EnemyMovement newEnemy = Instantiate(enemy, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+            newEnemy.transform.parent = gameObject.transform;
+            yield return new WaitForSeconds(secondsBetweenSpawn);
+        }
+    }
+}
