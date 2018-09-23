5a9b068d95789e34fe45b8786bba2d1dc008f616
zach <szboatwright@gmail.com>
2018-09-22 19:30:41 -0500

    ⚡  IMPROVE: [Realm Rush] enemies move smoothly

--- 5-realm-rush/Assets/EnemyMovement.cs
+++ 5-realm-rush/Assets/EnemyMovement.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    // Use this for initialization
+    // params
+    [SerializeField] int movementSmoothness = 3;
+    [SerializeField] float movementSpeed = 0.001f;
+
+    float waypointDistance;
+
     void Start() {
         PathFinder pathFinder = FindObjectOfType<PathFinder>();
+
         var path = pathFinder.GetPath();
-        StartCoroutine(FollowPath(path));
+        waypointDistance = Vector3.Distance(path[0].transform.position, path[1].transform.position) * movementSmoothness;
+
+        StartCoroutine(FollowPath(path, waypointDistance));
 	}
 
-    IEnumerator FollowPath(List<Waypoint> path)
+    IEnumerator FollowPath(List<Waypoint> path, float waypointDistance)
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
+            //transform.position = waypoint.transform.position;
             // loop that divides the distance between the two waypoints and increments the enemies distance between them based on how many seconds is being waited
-            yield return new WaitForSeconds(1);
+            Vector3 moveIncrement = (waypoint.transform.position - gameObject.transform.position) / waypointDistance;
+
+            for (int i = 0; i < waypointDistance; i++)
+            {
+                transform.position = gameObject.transform.position + moveIncrement;
+                yield return new WaitForSeconds(movementSpeed);
+            }
         }
     }
 
