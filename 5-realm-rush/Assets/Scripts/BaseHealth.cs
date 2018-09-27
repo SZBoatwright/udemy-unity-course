2565bc70a7ebb7e317495973f94ffdca6c8c56ce
zach <szboatwright@gmail.com>
2018-09-26 21:13:59 -0500

    📦  NEW: [Realm Rush] base health now in UI

--- 5-realm-rush/Assets/Scripts/BaseHealth.cs
+++ 5-realm-rush/Assets/Scripts/BaseHealth.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseHealth : MonoBehaviour {
 
     [SerializeField] int baseHealth = 10;
+    [SerializeField] Text healthText;
+
+    private void Start()
+    {
+        healthText.text = baseHealth.ToString();
+    }
 
     private void OnTriggerEnter()
     {
         print("collision");
         baseHealth -= 1;
+        healthText.text = baseHealth.ToString();
     }
 }
